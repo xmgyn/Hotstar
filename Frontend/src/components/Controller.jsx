baed216cec792f2163f47b4693fe9d43b9d46588
Mrigayan <connect.mrigayan@gmail.com>
2025-05-05 18:13:25 +0530

    Changes

--- Frontend/src/components/Controller.jsx
+++ Frontend/src/components/Controller.jsx
@@ -1,12 +1,43 @@
-function Controller() {
+import { useState } from "react";
+
+function Controller({ Props }) {
+  const video = Props.videoRef.current;
+
+  const setPlayState = () => {
+    if (Props.Play) {
+      video.pause();
+    } else {
+      video.play();
+    }
+    Props.setPlay(!Props.Play);
+  }
+
+  const setSeekState = (seekAhead) => {
+    if (seekAhead) {
+      video.currentTime += 10;
+    }
+    else {
+      video.currentTime -= 10;
+    }
+  }
+
+  const TimeFormat = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const secs = Math.floor(time % 60);
+
+    return `${hours > 0 ? hours.toString().padStart(2, '0') + ':' : ''}${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  }
+
   return (
     <div id="Controller" >
       <div className="Controller-Top">
-        <div className="Video-Current-Time oxygen-regular">26:00</div>
+        {video && <div className="Video-Current-Time nokora-bold">{TimeFormat(Props.currentTime)}</div>}
         <div className="Video-Seek-Bar">
+          <div className="Video-Seek-Line"></div>
           <div className="Video-Seek-Current"></div>
         </div>
-        <div className="Video-Total-Time oxygen-regular">1:32:00</div>
+        {video && <div className="Video-Total-Time nokora-bold">{TimeFormat(video.duration ?? 0)}</div>}
       </div>
       <div className="Controller-Bottom">
         <div className="Settings">
@@ -15,10 +46,9 @@ function Controller() {
         </div>
         <div className="Controls">
           <div className="Controls-Previous"><img src="/left-arrow.png" /></div>
-          <div className="Controls-Seek-Back"><img src="/previous.png" /></div>
-          <div className="Controls-Seek-Play"><img src="/play.png" /></div>
-          <div className="Controls-Seek-Pause"><img src="/pause.png" /></div>
-          <div className="Controls-Seek-Ahead"><img src="/next.png" /></div>
+          <div className="Controls-Seek-Back" onClick={() => setSeekState(0)}><img src="/previous.png" /></div>
+          {Props.Loading ? <div className="Controls-Seek-Loading"><img src="/play-loading.png" /></div> : <div className="Controls-Seek-Play" onClick={setPlayState}>{Props.Play ? <img src="/pause.png" /> : <img src="/play.png" />}</div>}
+          <div className="Controls-Seek-Ahead" onClick={() => setSeekState(1)}><img src="/next.png" /></div>
           <div className="Controls-Next"><img src="/forward.png" /></div>
         </div>
         <div className="Extra">
--- Frontend/src/routes/Play.jsx
+++ Frontend/src/routes/Play.jsx
@@ -1,9 +1,13 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Controller from '../components/Controller';
 
 import './Play.css';
 
 function Play({ id }) {
+    const [Play, setPlay] = useState(false); 
+    const [Loading, setLoading] = useState(true); 
+    const [currentTime, setCurrentTime] = useState(0);
+
     const videoRef = useRef(null); 
 
     useEffect(() => {
@@ -15,19 +19,38 @@ function Play({ id }) {
             hls.loadSource(videoSrc);
             hls.attachMedia(video);
 
-            hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                video.play();
-            });
+            hls.on(Hls.Events.MANIFEST_PARSED, () => { setLoading(false) });
         } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
             video.src = videoSrc;
             video.addEventListener('loadedmetadata', () => {
-                video.play();
+                setLoading(false);
             });
+            video.addEventListener('canplay', () => {
+                setLoading(false); 
+            });
+            
+            video.addEventListener('canplaythrough', () => {
+                setLoading(false); 
+            });
+            
         }
+
+        video.addEventListener("playing", () => { setLoading(false) });
+        video.addEventListener("seeked", () => { setLoading(false) });
+        video.addEventListener("waiting", () => { setLoading(true) });
+        video.addEventListener("seeking", () => { setLoading(true) });
+
+        video.addEventListener("timeupdate", () => { setCurrentTime(video.currentTime) });
 
         return () => {
             if (video && video.canPlayType('application/vnd.apple.mpegurl')) {
-                video.removeEventListener('loadedmetadata', () => video.play());
+                video.removeEventListener('loadedmetadata', () => setLoading(false));
+                video.removeEventListener('canplay', () => setLoading(false));
+                video.removeEventListener('canplaythrough', () => setLoading(false));
+                video.removeEventListener('playing', () => setLoading(false));
+                video.removeEventListener('seeked', () => setLoading(false));
+                video.removeEventListener('waiting', () => setLoading(false));
+                video.removeEventListener('seeking', () => setLoading(false));
             }
         };
     }, [id]);
@@ -44,7 +67,7 @@ function Play({ id }) {
                     default
                 />
             </video>
-            <Controller />
+            <Controller Props={{ Loading, Play, setPlay, videoRef, currentTime }} />
         </div>
     );
 }
